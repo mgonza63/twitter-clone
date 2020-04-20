692ed532fc18784912e33f75faabd47a60442fcd
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-19 23:42:45 -0500

    logged the data from the form as an object

---
+++ public/client.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+    
+    const tweet = {
+        name,
+        content
+    };
+    console.log(tweet);
+})
