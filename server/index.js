fb086ad63629cda3d4e0353ee6a461c40c854e0c
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-22 02:05:47 -0500

    added db and created the collection for the post request

--- server/index.js
+++ server/index.js
@@ -2,29 +2,44 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-
 const app = express();
 
+// db
+const monk = require('monk');
+const db = monk('localhost/twitter'); // twitter is the name of the db
+const tweets = db.get('tweets'); // name of the collection
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
+//GET request
 app.get('/', (req, res) => {
     res.json({
         message: 'hello 🐔'
     })
 });
 
+// Validation for the POST request
 function isTweetValid(tweet) {
-    return tweet.name && tweet.name.toString().trim !== '' &&
-        tweet.content && tweet.content.toString().trim !== ''
+    return tweet.name && tweet.name.toString().trim() !== '' &&
+        tweet.content && tweet.content.toString().trim() !== ''
 }
 
+// POST request
 app.post('/tweets', (req, res) => {
     if (isTweetValid(req.body)) {
         const tweet = {
             name: req.body.name.toString(),
             content: req.body.content.toString()
         };
+        
+        // calls the collection and adds the tweet to it with monk
+        tweets
+            .insert(tweet)
+            .then(createdTweet => {
+                res.json(createdTweet);
+            })
 
     } else {
         res.status(422);
