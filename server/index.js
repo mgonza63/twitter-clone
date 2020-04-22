0ef34c123d1ffb626dcc76226f7264a1047c3cad
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-22 01:26:24 -0500

    created get and post request, fetch from client to talk to server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'hello 🐔'
+    })
+});
+
+app.post('/tweets', (req, res) => {
+    console.log(req.body);
+})
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
