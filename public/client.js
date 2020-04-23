fc495ae10aea84fedffd62dad64716626c02f8e2
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-22 20:21:29 -0500

    deployed frontend

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form');
 const spinner = document.querySelector('.spinner');
-const API_URL = 'http://localhost:3000/tweets';
+const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000/tweets' : 'https://tweet-v2.herokuapp.com';
 
 const tweetsElement = document.querySelector('.tweets')
 
